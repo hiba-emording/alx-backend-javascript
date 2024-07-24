1cba66d228fb07295ee55ce152b016faf1e8a63a
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-07-24 19:11:03 +0000

    Fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => ({ status: 500, body: 'error' }))
+    .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
