2611e4faf942586abfee372815af544d216bc9de
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-07-25 17:44:44 +0000

    classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,3 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {                                                                                                                                this._maxStudentsSize = maxStudentsSize;
+  }                                                                                                                                                         }
