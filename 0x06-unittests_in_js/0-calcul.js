e012447f2f4b4c3e086337ebbd85cdda7a53fc45
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-09-20 15:43:53 +0300

    unit test in js

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,4 @@
+// 0-calcul.js
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,8 @@
+// 6-payment_token.js
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({data: 'Successful response from the API'});
+  }
+});
+
+module.exports = getPaymentTokenFromAPI;
